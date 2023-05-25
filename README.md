655a45009db22b668c95baa3b86eaa3fdc073e67
Kijun Shin <sharaelong.shin@gmail.com>
2023-05-25 22:19:30 +0900

    Fix chained KL naming into 2-opt

--- README.md
+++ README.md
@@ -10,12 +10,17 @@ Score: 32342713.38
 Time: 12ms, same as before  
 It uses every salesmen in tour.  
 
-3. Chained Lin-Kernighan heuristic  
+3. 2-opt heuristic  
 Score: 26611309.87  
 Time: 552ms  
-I applied new technique to find more improved TSP, no change to clustering. ChatGPT gives a (is it real correct code?) code about it. Also I made 32ms time limit for each salesman instance so it will not exceed time limit. However real time is 552ms which means every subproblem was solved under 32ms.  
+I applied new technique to find more improved TSP, no change to clustering. ChatGPT gives a code about it. Also I made 32ms time limit for each salesman instance so it will not exceed time limit. However real time is 552ms which means every subproblem was solved under 32ms.  
 
 4. Clustering with K-means  
 Score: 34219073.69  
 Time: 1120ms  
 I used k-means for clustering (which also given by ChatGPT), but it shows worse results than naive 10x14 grid. Max iteration was 100, and clustering takes time about 550ms purely. I don't have any intuition about how k-means groups points in random distribution...  
+
+5. 2-opt with random initial tour
+Score: 26519866.54  
+Time: 760ms  
+I called 2-opt function multiple time with random initial tour. But solution quality is not improved at much.  
--- main.cpp
+++ main.cpp
@@ -25,182 +25,120 @@ double tourLength(const vector<pll>& points, const vector<int>& tour) {
     return totalLength;
 }
 
-// Function to perform the Chained Lin-Kernighan algorithm
-vector<int> chainedLinKernighan(const vector<pll>& points, const chrono::milliseconds& timeLimit) {
+// Function to perform the 2-opt algorithm
+vector<int> twoopt(const vector<pll>& points, const chrono::milliseconds& timeLimit) {
     int n = points.size();
-
-    // Initialize the tour to a simple sequential order
-    vector<int> tour(n);
-    for (int i = 0; i < n; ++i) {
-        tour[i] = i;
-    }
 
     double bestLength = numeric_limits<double>::max();
     vector<int> bestTour;
 
-    // Calculate the initial tour length
-    double currentLength = tourLength(points, tour);
+    for (int it=0; it<9; ++it) {
+        // Initialize the tour to a simple sequential order
+        vector<int> tour(n);
+        for (int i = 0; i < n; ++i) {
+            tour[i] = i;
+        }
+        
+        // Shuffle the vector randomly
+        random_device rd;
+        mt19937 g(rd());
+        shuffle(tour.begin(), tour.end(), g);
 
-    // Get the current time
-    auto startTime = chrono::high_resolution_clock::now();
+        // Calculate the initial tour length
+        double currentLength = tourLength(points, tour);
 
-    // Main loop of the algorithm
-    while (true) {
-        bool improvement = false;
+        // Get the current time
+        auto startTime = chrono::high_resolution_clock::now();
 
-        // Try all possible 2-opt exchanges
-        for (int i = 0; i < n - 2; ++i) {
-            for (int j = i + 2; j < n; ++j) {
-                // Calculate the change in tour length
-                double delta = 0.0;
-                delta += distance(points[tour[i]], points[tour[j]]);
-                delta += distance(points[tour[i + 1]], points[tour[(j + 1) % n]]);
-                delta -= distance(points[tour[i]], points[tour[i + 1]]);
-                delta -= distance(points[tour[j]], points[tour[(j + 1) % n]]);
+        // Main loop of the algorithm
+        while (true) {
+            bool improvement = false;
 
-                // Update the tour length
-                double newLength = currentLength + delta;
+            // Try all possible 2-opt exchanges
+            for (int i = 0; i < n - 2; ++i) {
+                for (int j = i + 2; j < n; ++j) {
+                    // Calculate the change in tour length
+                    double delta = 0.0;
+                    delta += distance(points[tour[i]], points[tour[j]]);
+                    delta += distance(points[tour[i + 1]], points[tour[(j + 1) % n]]);
+                    delta -= distance(points[tour[i]], points[tour[i + 1]]);
+                    delta -= distance(points[tour[j]], points[tour[(j + 1) % n]]);
 
-                // Update the tour if the new length is better
-                if (newLength < bestLength) {
-                    bestLength = newLength;
-                    // Reverse the order of the tour segment between i and j
-                    vector<int> newTour = tour;
-                    reverse(newTour.begin() + i + 1, newTour.begin() + j + 1);
-                    bestTour = newTour;
-                    improvement = true;
+                    // Update the tour length
+                    double newLength = currentLength + delta;
+
+                    // Update the tour if the new length is better
+                    if (newLength < bestLength) {
+                        bestLength = newLength;
+                        // Reverse the order of the tour segment between i and j
+                        vector<int> newTour = tour;
+                        reverse(newTour.begin() + i + 1, newTour.begin() + j + 1);
+                        bestTour = newTour;
+                        improvement = true;
+                    }
                 }
             }
-        }
 
-        // Break the loop if no improvement is made or time limit is exceeded
-        auto currentTime = chrono::high_resolution_clock::now();
-        auto elapsedTime = chrono::duration_cast<chrono::milliseconds>(currentTime - startTime);
-        if (!improvement || elapsedTime > timeLimit) {
-            break;
-        }
+            // Break the loop if no improvement is made or time limit is exceeded
+            auto currentTime = chrono::high_resolution_clock::now();
+            auto elapsedTime = chrono::duration_cast<chrono::milliseconds>(currentTime - startTime);
+            if (!improvement || elapsedTime > timeLimit) {
+                break;
+            }
 
-        tour = bestTour;
-        currentLength = bestLength;
+            tour = bestTour;
+            currentLength = bestLength;
+        }
     }
 
     return bestTour;
 }
 
-// // Function to calculate the Euclidean distance between two points
-// double distance(const pll& p1, const pll& p2) {
-//     ll dx = p1.first - p2.first;
-//     ll dy = p1.second - p2.second;
-//     return sqrt(dx * dx + dy * dy);
-// }
-
-// Function to perform the K-means clustering algorithm
-vector<vector<pll>> kmeans(const vector<pll>& points, int k, int maxIterations) {
-    int n = points.size();
-
-    // Randomly initialize the centroids
-    vector<pll> centroids(k);
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<int> dist(0, n - 1);
-    for (int i = 0; i < k; ++i) {
-        centroids[i] = points[dist(gen)];
-    }
-
-    vector<int> assignments(n, 0); // Cluster assignments for each point
-    vector<int> counts(k, 0); // Number of points assigned to each cluster
-
-    // Main loop of the algorithm
-    int iterations = 0;
-    while (iterations < maxIterations) {
-        // Assign each point to the nearest centroid
-        for (int i = 0; i < n; ++i) {
-            double minDist = numeric_limits<double>::max();
-            int bestCluster = 0;
-
-            for (int j = 0; j < k; ++j) {
-                double dist = distance(points[i], centroids[j]);
-                if (dist < minDist) {
-                    minDist = dist;
-                    bestCluster = j;
-                }
-            }
-
-            assignments[i] = bestCluster;
-            counts[bestCluster]++;
-        }
-
-        // Update the centroids
-        for (int i = 0; i < k; ++i) {
-            centroids[i] = pll(0, 0);
-        }
-
-        for (int i = 0; i < n; ++i) {
-            centroids[assignments[i]].first += points[i].first;
-            centroids[assignments[i]].second += points[i].second;
-        }
-
-        for (int i = 0; i < k; ++i) {
-            if (counts[i] > 0) {
-                centroids[i].first /= counts[i];
-                centroids[i].second /= counts[i];
-            }
-        }
-
-        // Clear the counts
-        counts.assign(k, 0);
-
-        iterations++;
-    }
-
-    // Create the clusters
-    vector<vector<pll>> clusters(k);
-    for (int i = 0; i < n; ++i) {
-        clusters[assignments[i]].push_back(points[i]);
-    }
-
-    return clusters;
-}
-
 void solve() {
 #ifdef SHARAELONG
-    // freopen("data/data.in", "r", stdin);
+    freopen("data/data.in", "r", stdin);
 #endif
 
     int n, k;
     cin >> n >> k;
     n = 8000;
     k = 140;
-
-    map<pll, int> mp;
-    vector<pll> points(n);
+    
+    vector<pair<pll, int>> points(n);
     for (int i=0; i<n; ++i) {
         int x, y;
         cin >> x >> y;
-        points[i] = { x, y };
-        mp[points[i]] = i+1;
+        points[i] = { { x, y }, i+1 };
     }
 
     const int MAX_COORD = 814000;
 
-    // Set the number of maximum iterations
-    int maxIterations = 100;
+    set<int> s;
+    
+    int dx = (MAX_COORD + 10) / 10;
+    int dy = (MAX_COORD + 14) / 14;
+    for (int i=0; i<10; ++i) {
+        for (int j=0; j<14; ++j) {
+            vector<pll> local_points;
+            vector<int> ori_idx;
+            for (int l=0; l<n; ++l) {
+                auto[p, idx] = points[l];
+                auto[x, y] = p;
+                if (dx * i <= x && x < dx * (i+1) && dy * j <= y && y < dy * (j+1)) {
+                    local_points.push_back(p);
+                    ori_idx.push_back(idx);
+                }
+            }
 
-    // Run the K-means clustering algorithm
-    vector<vector<pll>> clusters = kmeans(points, k, maxIterations);
+            // Set the time limit to 32 milliseconds
+            chrono::milliseconds timeLimit(32);
 
-    for (int i=0; i<k; ++i) {
-        // Set the time limit as INF
-        chrono::milliseconds timeLimit(1000);
-        
-        // Run the Chained Lin-Kernighan algorithm
-        vector<int> tour = chainedLinKernighan(clusters[i], timeLimit);
-        
-        cout << tour.size() << ' ';
-        for (int idx: tour) {
-            cout << mp[clusters[i][idx]] << ' ';
+            // Run the Chained 2-opt algorithm
+            vector<int> tour = twoopt(local_points, timeLimit);
+            cout << tour.size() << ' ';
+            for (int idx: tour) cout << ori_idx[idx] << ' ';
+            cout << '\n';
         }
-        cout << '\n';
     }
 }
 
